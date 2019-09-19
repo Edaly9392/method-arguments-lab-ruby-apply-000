a8a5aa089bcfbfabd978b3692582cdf71b001836
Erick Daly <erick.daly@gmail.com>
2019-09-19 22:13:01 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # lib/introduction.rb
 
 def introduction(name)
@@ -7,3 +8,14 @@ end
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+# lib/introduction.rb
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+>>>>>>> 366cb6551af994070207b9cab811134346c9ad6a
